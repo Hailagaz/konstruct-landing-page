94b7106481b90c30ddb14eee0e944aa0aa686076
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2022-12-13 17:09:49 +0200

    Fix structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 function js() {
 	return src('src/js/*.js')
@@ -17,6 +18,7 @@ function css() {
 	return src('src/scss/*.scss')
 		.pipe(sass())
 		.pipe(concat('style.css'))
+		.pipe(cleanCSS())
 		.pipe(dest('build/'));
 }
 
