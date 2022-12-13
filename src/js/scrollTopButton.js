a1a9a500cdd27cad3b72c644d42b8db69baa78e5
Hailagaz <omentielvo.lumenn@gmail.com>
2022-12-13 12:28:02 +0200

    Add js

---
+++ src/js/scrollTopButton.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const scrollBtn = document.querySelector('.showbutton');
+	window.addEventListener("scroll", function (e) {
+		if (window.scrollY > 700) {
+			scrollBtn.classList.remove('showbutton__hidden');
+		} else if (window.scrollY < 700) {
+			scrollBtn.classList.add('showbutton__hidden');
+		}
+	});
+	scrollBtn.addEventListener("click", function (e) {
+		window.scrollTo(0, 0);
+	});
+});
